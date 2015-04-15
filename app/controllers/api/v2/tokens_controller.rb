13e407ad710361cd50b42581f1d1752148a7150b
Artem Chernikov <skullzeek@gmail.com>
2015-04-15 13:14:19 +0200

    Refactor tests and simplify controller

--- app/controllers/api/v2/tokens_controller.rb
+++ app/controllers/api/v2/tokens_controller.rb
@@ -1,21 +1,14 @@
 class Api::V2::TokensController < Api::BaseController
 
-  def show
-    authenticate_or_request_with_http_basic do |username,password|
-      resource = User.find_by_username(username)
-      if resource && resource.valid_password?(password)
-        sign_in :user, resource
-        @token = JwtToken.new(
-          account: params[:account],
-          service: params[:service],
-          scope: params[:scope]
-        )
-        respond_with(@token)
-      else
-        head 401
-      end
-    end
+  before_filter :authenticate_user!
 
+  def show
+    @token = JwtToken.new(
+        account: params[:account],
+        service: params[:service],
+        scope: params[:scope]
+    )
+    respond_with(@token)
   end
 
 end
--- spec/api/v2/token_spec.rb
+++ spec/api/v2/token_spec.rb
@@ -3,25 +3,32 @@ require 'rails_helper'
 describe '/v2/token' do
 
   describe 'get token' do
-    before :all do
-      @password = 'this is a test'
-      @user = create(:user, password: @password)
-      @env = {}
-     end
 
-    describe 'as invalid user' do
+    let(:auth_mech) { ActionController::HttpAuthentication::Basic }
+    let(:password) { 'this is a test' }
+    let(:user) { create(:user, password: password) }
+
+    let(:valid_auth_header) do
+      { 'HTTP_AUTHORIZATION' => auth_mech.encode_credentials(user.email, password) }
+    end
+
+    let(:invalid_auth_header) do
+      { 'HTTP_AUTHORIZATION' => auth_mech.encode_credentials(user.email, 'wrong_password') }
+    end
+
+    let(:nonexistent_auth_header) do
+      { 'HTTP_AUTHORIZATION' => auth_mech.encode_credentials('caesar', 'password') }
+    end
+
+    context 'as invalid user' do
 
       it 'denies access when the password is wrong' do
-        @env['HTTP_AUTHORIZATION'] = 'Basic ' + Base64::encode64(
-          "#{@user.username}:wrong password")
-        get v2_token_url, { service: 'test', account: 'account', scope: 'scope' }, @env
+        get v2_token_url, { service: 'test', account: 'account', scope: 'scope' }, invalid_auth_header
         expect(response.status).to eq 401
       end
 
       it 'denies access when the user does not exist' do
-        @env['HTTP_AUTHORIZATION'] = 'Basic ' + Base64::encode64(
-          "caesar:password")
-        get v2_token_url, { service: 'test', account: 'account', scope: 'scope' }, @env
+        get v2_token_url, { service: 'test', account: 'account', scope: 'scope' }, nonexistent_auth_header
         expect(response.status).to eq 401
       end
 
@@ -29,21 +36,18 @@ describe '/v2/token' do
         get v2_token_url, { service: 'test', account: 'account', scope: 'scope' }
         expect(response.status).to eq 401
       end
+
     end
 
-    describe 'as valid user' do
-      before :all do
-        @env['HTTP_AUTHORIZATION'] = 'Basic ' + Base64::encode64(
-          "#{@user.username}:#{@password}")
-      end
+    context 'as valid user' do
 
       it 'performs a request with given data' do
-        get v2_token_url, { service: 'test', account: 'account', scope: 'scope' }, @env
+        get v2_token_url, { service: 'test', account: 'account', scope: 'scope' }, valid_auth_header
         expect(response.status).to eq 200
       end
 
       it 'decoded payload should conform with params sent' do
-        get v2_token_url, { service: 'test', account: 'account', scope: 'repository:foo:push' }, @env
+        get v2_token_url, { service: 'test', account: 'account', scope: 'repository:foo:push' }, valid_auth_header
         token = JSON.parse(response.body)['token']
         payload = JWT.decode(token, nil, false, { leeway: 2 })[0]
         expect(payload['sub']).to eq 'account'
