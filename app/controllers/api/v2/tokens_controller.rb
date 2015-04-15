fe8742a9d57d2e740d924d6206c9dd7acc702642
Flavio Castelli <fcastelli@suse.com>
2015-04-15 11:09:08 +0200

    Token API: take HTTP basic auth in consideration

--- app/controllers/api/v2/tokens_controller.rb
+++ app/controllers/api/v2/tokens_controller.rb
@@ -1,12 +1,21 @@
 class Api::V2::TokensController < Api::BaseController
 
   def show
-    @token = JwtToken.new(
-      account: params[:account],
-      service: params[:service],
-      scope: params[:scope]
-    )
-    respond_with(@token)
+    authenticate_or_request_with_http_basic do |username,password|
+      resource = User.find_by_username(username)
+      if resource && resource.valid_password?(password)
+        sign_in :user, resource
+        @token = JwtToken.new(
+          account: params[:account],
+          service: params[:service],
+          scope: params[:scope]
+        )
+        respond_with(@token)
+      else
+        head 401
+      end
+    end
+
   end
 
 end
--- spec/api/v2/token_spec.rb
+++ spec/api/v2/token_spec.rb
@@ -3,21 +3,55 @@ require 'rails_helper'
 describe '/v2/token' do
 
   describe 'get token' do
+    before :all do
+      @password = 'this is a test'
+      @user = create(:user, password: @password)
+      @env = {}
+     end
 
-    it 'performs a request with given data' do
-      get v2_token_url, { service: 'test', account: 'account', scope: 'scope' }
-      expect(response.status).to eq 200
+    describe 'as invalid user' do
+
+      it 'denies access when the password is wrong' do
+        @env['HTTP_AUTHORIZATION'] = 'Basic ' + Base64::encode64(
+          "#{@user.username}:wrong password")
+        get v2_token_url, { service: 'test', account: 'account', scope: 'scope' }, @env
+        expect(response.status).to eq 401
+      end
+
+      it 'denies access when the user does not exist' do
+        @env['HTTP_AUTHORIZATION'] = 'Basic ' + Base64::encode64(
+          "caesar:password")
+        get v2_token_url, { service: 'test', account: 'account', scope: 'scope' }, @env
+        expect(response.status).to eq 401
+      end
+
+      it 'denies access when basic auth credentials are not defined' do
+        get v2_token_url, { service: 'test', account: 'account', scope: 'scope' }
+        expect(response.status).to eq 401
+      end
     end
 
-    it 'decoded payload should conform with params sent' do
-      get v2_token_url, { service: 'test', account: 'account', scope: 'repository:foo:push' }
-      token = JSON.parse(response.body)['token']
-      payload = JWT.decode(token, nil, false, { leeway: 2 })[0]
-      expect(payload['sub']).to eq 'account'
-      expect(payload['aud']).to eq 'test'
-      expect(payload['access'][0]['type']).to eq 'repository'
-      expect(payload['access'][0]['name']).to eq 'foo'
-      expect(payload['access'][0]['actions'][0]).to eq 'push'
+    describe 'as valid user' do
+      before :all do
+        @env['HTTP_AUTHORIZATION'] = 'Basic ' + Base64::encode64(
+          "#{@user.username}:#{@password}")
+      end
+
+      it 'performs a request with given data' do
+        get v2_token_url, { service: 'test', account: 'account', scope: 'scope' }, @env
+        expect(response.status).to eq 200
+      end
+
+      it 'decoded payload should conform with params sent' do
+        get v2_token_url, { service: 'test', account: 'account', scope: 'repository:foo:push' }, @env
+        token = JSON.parse(response.body)['token']
+        payload = JWT.decode(token, nil, false, { leeway: 2 })[0]
+        expect(payload['sub']).to eq 'account'
+        expect(payload['aud']).to eq 'test'
+        expect(payload['access'][0]['type']).to eq 'repository'
+        expect(payload['access'][0]['name']).to eq 'foo'
+        expect(payload['access'][0]['actions'][0]).to eq 'push'
+      end
     end
 
   end
