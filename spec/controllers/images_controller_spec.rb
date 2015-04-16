ac79c9c8aed5e1908618665ce79afa30b7f348f9
Flavio Castelli <fcastelli@suse.com>
2015-04-16 22:06:10 +0200

    Code cleanup

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -20,139 +20,24 @@ require 'rails_helper'
 
 RSpec.describe ImagesController, type: :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Image. As you add validations to Image, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # ImagesController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "assigns all images as @images" do
-      image = Image.create! valid_attributes
+  describe 'GET #index' do
+    it 'assigns all images as @images' do
+      image = create(:image)
       get :index, {}, valid_session
       expect(assigns(:images)).to eq([image])
     end
   end
 
-  describe "GET #show" do
-    it "assigns the requested image as @image" do
-      image = Image.create! valid_attributes
+  describe 'GET #show' do
+    it 'assigns the requested image as @image' do
+      image = create(:image)
       get :show, {:id => image.to_param}, valid_session
       expect(assigns(:image)).to eq(image)
-    end
-  end
-
-  describe "GET #new" do
-    it "assigns a new image as @image" do
-      get :new, {}, valid_session
-      expect(assigns(:image)).to be_a_new(Image)
-    end
-  end
-
-  describe "GET #edit" do
-    it "assigns the requested image as @image" do
-      image = Image.create! valid_attributes
-      get :edit, {:id => image.to_param}, valid_session
-      expect(assigns(:image)).to eq(image)
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Image" do
-        expect {
-          post :create, {:image => valid_attributes}, valid_session
-        }.to change(Image, :count).by(1)
-      end
-
-      it "assigns a newly created image as @image" do
-        post :create, {:image => valid_attributes}, valid_session
-        expect(assigns(:image)).to be_a(Image)
-        expect(assigns(:image)).to be_persisted
-      end
-
-      it "redirects to the created image" do
-        post :create, {:image => valid_attributes}, valid_session
-        expect(response).to redirect_to(Image.last)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns a newly created but unsaved image as @image" do
-        post :create, {:image => invalid_attributes}, valid_session
-        expect(assigns(:image)).to be_a_new(Image)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, {:image => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested image" do
-        image = Image.create! valid_attributes
-        put :update, {:id => image.to_param, :image => new_attributes}, valid_session
-        image.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "assigns the requested image as @image" do
-        image = Image.create! valid_attributes
-        put :update, {:id => image.to_param, :image => valid_attributes}, valid_session
-        expect(assigns(:image)).to eq(image)
-      end
-
-      it "redirects to the image" do
-        image = Image.create! valid_attributes
-        put :update, {:id => image.to_param, :image => valid_attributes}, valid_session
-        expect(response).to redirect_to(image)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns the image as @image" do
-        image = Image.create! valid_attributes
-        put :update, {:id => image.to_param, :image => invalid_attributes}, valid_session
-        expect(assigns(:image)).to eq(image)
-      end
-
-      it "re-renders the 'edit' template" do
-        image = Image.create! valid_attributes
-        put :update, {:id => image.to_param, :image => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested image" do
-      image = Image.create! valid_attributes
-      expect {
-        delete :destroy, {:id => image.to_param}, valid_session
-      }.to change(Image, :count).by(-1)
-    end
-
-    it "redirects to the images list" do
-      image = Image.create! valid_attributes
-      delete :destroy, {:id => image.to_param}, valid_session
-      expect(response).to redirect_to(images_url)
     end
   end
 
--- spec/routing/repositories_routing_spec.rb
+++ spec/routing/repositories_routing_spec.rb
@@ -1,35 +1,13 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe RepositoriesController, type: :routing do
-  describe "routing" do
-
-    it "routes to #index" do
-      expect(:get => "/repositories").to route_to("repositories#index")
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(:get => '/repositories').to route_to('repositories#index')
     end
 
-    it "routes to #new" do
-      expect(:get => "/repositories/new").to route_to("repositories#new")
+    it 'routes to #show' do
+      expect(:get => '/repositories/1').to route_to('repositories#show', :id => '1')
     end
-
-    it "routes to #show" do
-      expect(:get => "/repositories/1").to route_to("repositories#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/repositories/1/edit").to route_to("repositories#edit", :id => "1")
-    end
-
-    it "routes to #create" do
-      expect(:post => "/repositories").to route_to("repositories#create")
-    end
-
-    it "routes to #update" do
-      expect(:put => "/repositories/1").to route_to("repositories#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/repositories/1").to route_to("repositories#destroy", :id => "1")
-    end
-
   end
 end
