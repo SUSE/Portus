72a71d3f831636e37851246d2ae65fac040ec62d
Miquel Sabaté <mikisabate@gmail.com>
2015-04-27 08:02:24 +0200

    policies: Update the policy to allow team members to push

--- spec/policies/namespace_policy_spec.rb
+++ spec/policies/namespace_policy_spec.rb
@@ -4,9 +4,13 @@ describe NamespacePolicy do
 
   subject { described_class }
 
-  let(:user) { build(:user) }
-  let(:team) { build(:team, owner: user) }
-  let(:namespace) { build(:namespace, team: team) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:team) do
+    t = FactoryGirl.create(:team, owner: user)
+    t.users = [user]
+    t
+  end
+  let(:namespace) { FactoryGirl.create(:namespace, team: team) }
 
   permissions :pull? do
 
@@ -24,6 +28,12 @@ describe NamespacePolicy do
 
     it 'allows access to user who is the owner of the team behind namespace' do
       expect(subject).to permit(user, namespace)
+    end
+
+    it 'allows access to users that are members of the team behind a namespace' do
+      user2 = FactoryGirl.create(:user)
+      team.users = [user, user2]
+      expect(subject).to permit(user2, namespace)
     end
 
     it 'disallows access to user who is not the owner of the team behind namespace' do
