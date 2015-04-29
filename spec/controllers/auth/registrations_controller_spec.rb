dd23860457d2a36774d72dd38515e3cac93274b2
Flavio Castelli <fcastelli@suse.com>
2015-04-29 19:38:54 +0200

    Fix test broken with ruby 2.1.3

--- spec/controllers/auth/registrations_controller_spec.rb
+++ spec/controllers/auth/registrations_controller_spec.rb
@@ -12,21 +12,21 @@ describe Auth::RegistrationsController do
 
     it 'defaults admin to false when omitted' do
       post :create, user: {
-        username: 'portus',
-        email: 'portus@test.com',
-        password: '12341234',
-        'password_confirmation': '12341234'
+        'username' => 'portus',
+        'email' => 'portus@test.com',
+        'password' => '12341234',
+        'password_confirmation' => '12341234'
       }
       expect(User.find_by!(username: 'portus')).not_to be_admin
     end
 
     it 'handles the admin column properly' do
       post :create, user: {
-        username: 'portus',
-        email: 'portus@test.com',
-        password: '12341234',
-        'password_confirmation': '12341234',
-        admin: true
+        'username' => 'portus',
+        'email' =>  'portus@test.com',
+        'password' =>  '12341234',
+        'password_confirmation' => '12341234',
+        'admin' => true
       }
       expect(User.find_by!(username: 'portus')).to be_admin
     end
@@ -34,11 +34,11 @@ describe Auth::RegistrationsController do
     it 'omits the value of admin if there is already another admin' do
       create(:user, admin: true)
       post :create, user: {
-        username: 'portus',
-        email: 'portus@test.com',
-        password: '12341234',
-        'password_confirmation': '12341234',
-        admin: true
+        'username'=> 'portus',
+        'email' => 'portus@test.com',
+        'password' => '12341234',
+        'password_confirmation' => '12341234',
+        'admin' => true
       }
       expect(User.find_by!(username: 'portus')).not_to be_admin
     end
