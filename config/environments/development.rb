9d4f1bef3b9c25488b17fa18380ad69c8bcb0b99
Flavio Castelli <fcastelli@suse.com>
2015-05-06 16:15:27 +0200

    development: make it easier to debug assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  config.assets.debug = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
