d8378c7d7d5be62f1665311b847c51df990a5122
Miquel Sabaté Solà <msabate@suse.de>
2015-06-08 14:18:16 +0200

    Merge the values from the config.yml and the config-local.yml files

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -2,11 +2,18 @@
 config = File.join(Rails.root, 'config', 'config.yml')
 local = File.join(Rails.root, 'config', 'config-local.yml')
 
-APP_CONFIG =
+app_config = YAML.load_file(config)['settings'] || {}
+
 if File.exist?(local)
-  YAML.load_file(local)['settings']
-elsif File.exist?(config)
-  YAML.load_file(config)['settings']
-else
-  raise StandardError, 'You need a config.yml file!'
+  local_config = YAML.load_file(local)['settings'] || {}
+
+  app_config = app_config.merge(local_config) do |_, global, local|
+    if global.is_a?(Hash) && local.is_a?(Hash)
+      global.merge(local)
+    else
+      local
+    end
+  end
 end
+
+APP_CONFIG = app_config
