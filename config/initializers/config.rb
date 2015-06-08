af67ed16af23c34b62de74ff79051940bb90fd06
Miquel Sabaté Solà <msabate@suse.de>
2015-06-08 11:54:16 +0200

    Make possible to disable Gravatar
    This commit also introduces the config.yml file. This file is ignored by git,
    and if the admin hasn't configured it yet, this application will take some
    defaults automatically.
    
    Fixes #76

---
+++ config/initializers/config.rb
@@ -0,0 +1,12 @@
+
+path = File.join(Rails.root, 'config', 'config.yml')
+
+APP_CONFIG =
+if File.exist?(path)
+  YAML.load_file(path)['settings']
+else
+  # If the config.yml file does not exist, go on with some defaults.
+  {
+    'gravatar': true
+  }
+end
