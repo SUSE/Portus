938aa29f9dccb077d2ce4807f2e7c72bbb04818e
Miquel Sabaté Solà <msabate@suse.de>
2015-06-08 14:42:20 +0200

    Make some extra checks for the config-local.yml file

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -5,15 +5,13 @@ local = File.join(Rails.root, 'config', 'config-local.yml')
 app_config = YAML.load_file(config)['settings'] || {}
 
 if File.exist?(local)
-  local_config = YAML.load_file(local)['settings'] || {}
-
-  app_config = app_config.merge(local_config) do |_, global, local|
-    if global.is_a?(Hash) && local.is_a?(Hash)
-      global.merge(local)
-    else
-      local
-    end
+  # Check for bad user input in the local config.yml file.
+  local_config = YAML.load_file(local)['settings']
+  if local_config.nil? || !local_config.is_a?(Hash)
+    raise StandardError, 'Wrong format for the config-local file!'
   end
+
+  app_config = app_config.merge(local_config)
 end
 
 APP_CONFIG = app_config
