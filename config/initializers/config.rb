4ac860e3cff7749c34d46310f8d9d9d27da47995
Miquel Sabaté Solà <msabate@suse.de>
2015-06-08 13:54:47 +0200

    Write the defaults in the config.yml file, and accept a config-local.yml file
    The config-local.yml file takes precedence before the config.yml, and it's
    ignored by git.

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,12 +1,12 @@
 
-path = File.join(Rails.root, 'config', 'config.yml')
+config = File.join(Rails.root, 'config', 'config.yml')
+local = File.join(Rails.root, 'config', 'config-local.yml')
 
 APP_CONFIG =
-if File.exist?(path)
-  YAML.load_file(path)['settings']
+if File.exist?(local)
+  YAML.load_file(local)['settings']
+elsif File.exist?(config)
+  YAML.load_file(config)['settings']
 else
-  # If the config.yml file does not exist, go on with some defaults.
-  {
-    'gravatar' => true
-  }
+  raise StandardError, 'You need a config.yml file!'
 end
