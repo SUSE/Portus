a90a7260d6262e5663e8616522347233decdd09a
Miquel Sabaté Solà <msabate@suse.de>
2015-06-15 14:16:14 +0200

    spec: specified the expected error in the team_spec

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -7,7 +7,7 @@ describe Team do
   it { should have_many(:namespaces) }
 
   it 'checks whether the given name is downcased or not' do
-    expect { FactoryGirl.create(:team, name: 'TeAm') }.to raise_error
+    expect { FactoryGirl.create(:team, name: 'TeAm') }.to raise_error(ActiveRecord::RecordNotSaved)
     expect { FactoryGirl.create(:team, name: 'team') }.not_to raise_error
   end
 
