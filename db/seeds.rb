e8fee3f7e66825cd0bcd6ef97e3aa733f560e87d
Miquel Sabaté <mikisabate@gmail.com>
2015-06-22 12:05:11 +0200

    rubocop: added cops for rails
    The reported issues have been:
    
    - Since 4.2 *_filter methods are discouraged, in favor of *_action methods.
    - We should use Rails.logger instead of puts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 if ENV['INTEGRATION_TESTS']
-  puts 'Adding user username31'
+  Rails.logger.info 'Adding user username31'
   if (User.find_by_username('username31'))
-    puts 'User already exists. Please drop the database first'
+    Rails.logger.fatal 'User already exists. Please drop the database first'
     exit -1
   end
   User.create(
@@ -11,7 +11,7 @@ if ENV['INTEGRATION_TESTS']
     admin: true
   )
 
-  puts 'Adding registry portus.suse.example.com:5000'
+  Rails.logger.info 'Adding registry portus.suse.example.com:5000'
   Registry.create(
     name: 'portus.suse.example.com:5000',
     hostname: 'portus.suse.example.com:5000'
