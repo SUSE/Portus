SUSE/Portus

commit 281e29e14c859b19207433d0c28c963cc5ed19c0

Starting to use PhantomJS to write acceptance tests
For now I've added a test when the user is creating a namespace from the team's
page. There are a couple of hacks with this test that need to be fixed.

spec/support/database_cleaner.rb
@@ -1,0 +1,31 @@
+
+require 'database_cleaner'
+
+# DatabaseCleaner has been configured like this:
+#
+#   - The database gets truncated before starting the test suite.
+#   - The cleaning strategy for the tests is by transaction, but tests dealing
+#     with the UI have to go with truncation. This is a requirement from the
+#     Poltergeist gem.
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end

spec/features/teams_spec.rb
@@ -1,0 +1,34 @@
+require 'rails_helper'
+
+feature 'Teams support', focus: true do
+  let!(:registry) { create(:registry) }
+  let!(:user) { create(:user, admin: true) }
+  let!(:team) { create(:team, owners: [user]) }
+
+  before do
+    login_as user, scope: :user
+  end
+
+  scenario 'A namespace can be created from the team page', js: true do
+    visit team_path(team)
+
+    # The form appears after clicking the "Add namespace" link.
+    expect(find('#add_namespace_form', visible: false)).to_not be_visible
+    find('#add_namespace_btn').click
+    expect(find('#add_namespace_form')).to be_visible
+    sleep 1
+    id = page.evaluate_script('document.activeElement.id')
+    expect(id).to eq 'namespace_namespace'
+
+    # Fill the form and wait for the AJAX response.
+    fill_in 'Namespace', with: 'new-namespace'
+    click_button 'Add'
+    wait_for_ajax
+
+    # See the response.
+    namespace = Namespace.find_by(name: 'new-namespace')
+    expect(page).to have_css("#namespace_#{namespace.id}")
+    sleep 2
+    expect(find('#add_namespace_form', visible: false)).to_not be_visible
+  end
+end

spec/rails_helper.rb
@@ -1,72 +1,27 @@
+
 ENV['RAILS_ENV'] ||= 'test'
+
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'devise'
-require 'capybara/poltergeist'
-require 'capybara/rails'
-require 'capybara/rspec'
-require 'database_cleaner'
 require 'ffaker'
 require 'factory_girl_rails'
 require 'pundit/rspec'
 
-# Schema and seeds handling
-# ActiveRecord::Schema.verbose = false
+# Raise exception for pending migrations after reading the schema.
 ActiveRecord::Migration.maintain_test_schema!
 
-Capybara.server do |app, port|
-  require 'rack/handler/thin'
-  Rack::Handler::Thin.run(app, Port: port)
-end
-
-# This is debug bundle. Use when more details needed
-# Rails.logger.level = 0
-# Capybara.register_driver :poltergeist do |app|
-#   Capybara::Poltergeist::Driver.new(app, debug: true, js_errors: true, phantomjs_options: ['--load-images=no'])
-# end
-
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, phantomjs_options: ['--load-images=no'])
-end
-
-Capybara.configure do |config|
-  config.javascript_driver = :poltergeist
-  config.default_wait_time = 5
-  config.match = :one
-  config.exact_options = true
-  config.ignore_hidden_elements = true
-  config.visible_text_only = true
-  config.default_selector = :css
-end
-
+# All the configuration that is specific for a gem (or set of related gems) has
+# been pushed into individual files inside the `spec/support` directory.
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-DatabaseCleaner.clean_with(:truncation)
-
 RSpec.configure do |config|
+  # If we want Capybara + DatabaseCleaner + Poltergeist to work correctly, we
+  # have to just set this to false.
   config.use_transactional_fixtures = false
+
   config.infer_spec_type_from_file_location!
-  config.include Devise::TestHelpers, type: :controller
-  config.include Devise::TestHelpers, type: :helper
   config.include FactoryGirl::Syntax::Methods
   config.infer_base_class_for_anonymous_controllers = true
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.cleaning do
-      factories_to_lint = FactoryGirl.factories.reject do |factory|
-        factory.name =~ /raw_.*_event/
-      end
-      FactoryGirl.lint factories_to_lint
-    end
-  end
-
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
-
 end

spec/support/capybara.rb
@@ -1,0 +1,24 @@
+
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(
+    app,
+    js_errors: false,
+    phantomjs_options: ['--load-images=no']
+  )
+end
+
+Capybara.javascript_driver = :poltergeist
+
+Capybara.configure do |config|
+  config.javascript_driver = :poltergeist
+  config.default_wait_time = 5
+  config.match = :one
+  config.exact_options = true
+  config.ignore_hidden_elements = true
+  config.visible_text_only = true
+  config.default_selector = :css
+end

spec/support/devise.rb
@@ -1,0 +1,10 @@
+
+# Setup devise for tests.
+RSpec.configure do |config|
+  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::TestHelpers, type: :helper
+
+  # Needed for methods such as `login_as`.
+  config.include Warden::Test::Helpers
+  config.before(:suite) { Warden.test_mode! }
+end

spec/support/wait_for_ajax.rb
@@ -1,0 +1,27 @@
+
+# Theoretically, Capybara is clever enough to wait for asynchronous events to
+# happen (e.g. AJAX). Sadly, this is not always true. For more, read:
+#
+#   https://robots.thoughtbot.com/automatically-wait-for-ajax-with-capybara
+#
+# Therefore, we add the "wait_for_ajax" method, that will be used for these
+# corner cases.
+module WaitForAjax
+  # Wait for all the AJAX requests to have concluded. It respects the timeout
+  # defined by `Capybara.default_wait_time`.
+  def wait_for_ajax
+    Timeout.timeout(Capybara.default_wait_time) do
+      loop until finished_all_ajax_requests?
+    end
+  end
+
+  private
+
+  def finished_all_ajax_requests?
+    page.evaluate_script('jQuery.active').zero?
+  end
+end
+
+RSpec.configure do |config|
+  config.include WaitForAjax, type: :feature
+end

commit 33c0437f68daad47798d1f3498463e86b7df9584

spec: lint factories before running the test suite
Regression from: 281e29e14c859b19207433d0c28c963cc5ed19c0

spec/support/database_cleaner.rb
@@ -11,6 +11,12 @@ require 'database_cleaner'
 RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with :truncation
+    DatabaseCleaner.cleaning do
+      factories_to_lint = FactoryGirl.factories.reject do |factory|
+        factory.name =~ /raw_.*_event/
+      end
+      FactoryGirl.lint factories_to_lint
+    end
   end
 
   config.before(:each) do