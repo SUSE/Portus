f2f5cfc5f4112f38438d8166dbef1194ad670249
Cynthia Sanchez <csanchez@suse.com>
2015-07-14 15:25:48 +0200

    New: wow animation implemented. Animation on Docker logo. new section: why use portus

--- assets/js/portus.js
+++ assets/js/portus.js
@@ -1,8 +1,29 @@
+//reset the scroll to 0 (top of page)
+$(window).on('beforeunload', function() {
+  $(window).scrollTop(0);
+});
 
-$(function () {
-  wow = new WOW(
-    {
-      offset: 50
-    }
-  )
+//init WOW
+wow = new WOW(
+  {
+    offset: 30
+  }
+)
+wow.init();
+
+$(window).bind('scroll',function(e) {
+  dockerIntoPortu()
 })
+
+var dockerIntoPortu = function () {
+
+  var element = $(".fixit-container");
+  var scrolledTop = element.offset().top - $(window).scrollTop();
+
+  //when the container reaches the top of the window, start the animations
+  if (scrolledTop < 0 ) {
+
+    $(".fixit-element").addClass('animated zoomOutDown')
+
+  }
+}
