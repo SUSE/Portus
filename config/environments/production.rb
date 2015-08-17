4060645b2fb88bf793bd7b933833a6606b72016a
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2015-08-17 13:54:04 +0200

    fix log level in production
    We had the log level as debug. This was by mistake

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  # config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [:subdomain, :uuid]
