25410647ca59afe4d9b78b152374d36a3d2fb001
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2015-08-20 20:09:51 +0200

    [packaging] add scripts for configuring portus, registry and ssl

---
+++ packaging/suse/scripts/configure_portus.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+bundle="/srv/Portus/vendor/bundle/ruby/2.1.0/bin/bundler.ruby2.1"
+export GEM_PATH=/srv/Portus/vendor/bundle/ruby/2.1.0/
+pushd /srv/Portus
+
+export SKIP_MIGRATION=yes
+export RAILS_ENV=production
+cp /srv/Portus/packaging/suse/conf/apache2/vhosts.d/portus.conf.in /etc/apache2/vhosts.d/portus.conf
+echo "Generating secrets"
+SECRET=$($bundle exec rake secret)
+sed -e "s/__SECRET_KEY__/$SECRET/g" -i /etc/apache2/vhosts.d/portus.conf
+
+echo "Create database"
+$bundle exec rake db:create
+
+echo "Set pasword"
+PP=$(echo $RANDOM)
+sed -e "s/__PORTUS_PASSWORD__/$PP/g" -i /etc/apache2/vhosts.d/portus.conf
+
+popd
+
