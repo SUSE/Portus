7719c9aaa2c5bad1a940621b8cc14e1a3bd84596
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2015-08-25 19:05:33 +0200

    do not exit when running database migrations on production but
    instead show the error page since any access to the database will
    raise an exception
    
    This way we have a running application that states the configuration
    errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,7 @@ class ErrorsController < ApplicationController
   skip_before_filter :check_requirements
   skip_before_filter :authenticate_user!
   def show
-    @fix_database = env["action_dispatch.exception"].class == ActiveRecord::NoDatabaseError
+    @fix_database = env["action_dispatch.exception"].class.name.starts_with? "ActiveRecord"
     @fix_ssl = params[:fix_ssl] == "true"
     @fix_secrets = params[:fix_secrets] == "true"
     render :layout => false
