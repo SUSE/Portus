72f830ca1a10b25eca4365a11e115d5c31b5addc
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2015-08-31 19:41:19 +0200

    [packaging] review configure_registry script
    So that it prompts the user if he is sure, since this will overwrite
    local configuration, plus it is not needed if the registry is not in
    this machine.
    
    It will also create a backup copy so that this step can be rolled back.

--- packaging/suse/scripts/configure_registry.sh
+++ packaging/suse/scripts/configure_registry.sh
@@ -3,6 +3,15 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 echo "Configuring registry ..."
+echo "Do you want to configure the local registry with portus?"
+echo "If you answer 'yes', the local registry configuration will be overwritten"
+read answer
+if [ "$answer" != "yes" ];then
+  exit
+fi
+
 HOSTNAME=$(cat /etc/HOSTNAME)
+echo "Create backup at /etc/registry/config.yml.portus.back"
+cp /etc/registry/config.yml /etc/registry/config.yml.portus.back
 sed -e "s/__HOSTNAME__/$HOSTNAME/g" /srv/Portus/packaging/suse/conf/registry/config.yml.in > /etc/registry/config.yml
 
