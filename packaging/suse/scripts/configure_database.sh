35a39538135453a1ba4a1ef2d39439741b2fa0ea
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2015-08-31 19:32:54 +0200

    [packaging] set defaults to database configuration

--- packaging/suse/scripts/configure_database.sh
+++ packaging/suse/scripts/configure_database.sh
@@ -7,6 +7,7 @@ if [ ! -f /etc/apache2/vhosts.d/portus.conf ];then
 fi
 
 echo "Configuring database connection"
+echo "Left blank and press enter to set up defaults"
 
 echo "Enter username"
 read db_username
@@ -14,22 +15,33 @@ echo "Enter password"
 read db_password
 echo "Enter host"
 read db_host
-echo "Enter database"
+echo "Enter database(portus_production)"
 read db_name
-# enable option
-sed -e "s/# SetEnv PORTUS_PRODUCTION_USERNAME/SetEnv PORTUS_PRODUCTION_USERNAME/g" -i /etc/apache2/vhosts.d/portus.conf
-# set option
-sed -e "s/SetEnv PORTUS_PRODUCTION_USERNAME.*/SetEnv PORTUS_PRODUCTION_USERNAME $db_username/g" -i /etc/apache2/vhosts.d/portus.conf
 
-# enable option
-sed -e "s/# SetEnv PORTUS_PRODUCTION_PASSWORD/SetEnv PORTUS_PRODUCTION_PASSWORD/g" -i /etc/apache2/vhosts.d/portus.conf
-# set option
-sed -e "s/SetEnv PORTUS_PRODUCTION_PASSWORD.*/SetEnv PORTUS_PRODUCTION_PASSWORD $db_password/g" -i /etc/apache2/vhosts.d/portus.conf
+if [ "$db_name" == "" ];then
+  db_name="portus_production"
+fi
 
-# enable option
-sed -e "s/# SetEnv PORTUS_PRODUCTION_HOST/SetEnv PORTUS_PRODUCTION_HOST/g" -i /etc/apache2/vhosts.d/portus.conf
-# set option
-sed -e "s/SetEnv PORTUS_PRODUCTION_HOST.*/SetEnv PORTUS_PRODUCTION_HOST $db_host/g" -i /etc/apache2/vhosts.d/portus.conf
+if [ "$db_username" != "" ];then
+  # enable option
+  sed -e "s/# SetEnv PORTUS_PRODUCTION_USERNAME/SetEnv PORTUS_PRODUCTION_USERNAME/g" -i /etc/apache2/vhosts.d/portus.conf
+  # set option
+  sed -e "s/SetEnv PORTUS_PRODUCTION_USERNAME.*/SetEnv PORTUS_PRODUCTION_USERNAME $db_username/g" -i /etc/apache2/vhosts.d/portus.conf
+fi
+
+if [ "$db_password" == "" ];then
+  # enable option
+  sed -e "s/# SetEnv PORTUS_PRODUCTION_PASSWORD/SetEnv PORTUS_PRODUCTION_PASSWORD/g" -i /etc/apache2/vhosts.d/portus.conf
+  # set option
+  sed -e "s/SetEnv PORTUS_PRODUCTION_PASSWORD.*/SetEnv PORTUS_PRODUCTION_PASSWORD $db_password/g" -i /etc/apache2/vhosts.d/portus.conf
+fi
+
+if [ "$db_host" == "" ];then
+  # enable option
+  sed -e "s/# SetEnv PORTUS_PRODUCTION_HOST/SetEnv PORTUS_PRODUCTION_HOST/g" -i /etc/apache2/vhosts.d/portus.conf
+  # set option
+  sed -e "s/SetEnv PORTUS_PRODUCTION_HOST.*/SetEnv PORTUS_PRODUCTION_HOST $db_host/g" -i /etc/apache2/vhosts.d/portus.conf
+fi
 
 # enable option
 sed -e "s/# SetEnv PORTUS_PRODUCTION_DATABASE/SetEnv PORTUS_PRODUCTION_HOST/g" -i /etc/apache2/vhosts.d/portus.conf
