6355e46e62f73b24bbb8847d42435268982e287d
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2015-08-31 19:44:26 +0200

    [packaging] review configure_registry
    if registry configuration does not exist, skip configuration

--- packaging/suse/scripts/configure_registry.sh
+++ packaging/suse/scripts/configure_registry.sh
@@ -3,6 +3,14 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 echo "Configuring registry ..."
+
+if [ ! -f /etc/registry/config.yml ];then
+  echo "Can't find local registry configuration"
+  echo "/etc/registry/config.yml does not exist"
+  echo "Skipping registry configuration"
+  exit
+fi
+
 echo "Do you want to configure the local registry with portus?"
 echo "If you answer 'yes', the local registry configuration will be overwritten"
 read answer
