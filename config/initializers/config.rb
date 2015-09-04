ed86eec96902f7ba6586bd1a5ddd82d09f35168a
Miquel Sabaté Solà <msabate@suse.com>
2015-09-04 15:09:46 +0200

    config: move the configuration handling into its own class
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,34 +1,4 @@
-
-# TODO: (mssola) move this into its own file in the `lib` directory.
-# TODO: (mssola) take advantage of YAML syntax for inheriting values. This way
-# we could define different values for different environments (useful for
-# testing).
-
-config = File.join(Rails.root, "config", "config.yml")
-local = File.join(Rails.root, "config", "config-local.yml")
-
-app_config = YAML.load_file(config) || {}
-
-if File.exist?(local)
-  # Check for bad user input in the local config.yml file.
-  local_config = YAML.load_file(local)
-  if local_config.nil? || !local_config.is_a?(Hash)
-    raise StandardError, "Wrong format for the config-local file!"
-  end
-
-  app_config = app_config.merge(local_config)
-end
-
-class << app_config
-  # The `enabled?` method is a convenient method that checks whether a specific
-  # feature is enabled or not. This method takes advantage of the convention
-  # that each feature has the "enabled" key inside of it. If this key exists in
-  # the checked feature, and it's set to true, then this method will return
-  # true. It returns false otherwise.
-  def enabled?(feature)
-    return false if !self[feature] || self[feature].empty?
-    self[feature]["enabled"].eql?(true)
-  end
-end
-
-APP_CONFIG = app_config
+default    = File.join(Rails.root, "config", "config.yml")
+local      = File.join(Rails.root, "config", "config-local.yml")
+cfg        = Portus::Config.new(default, local)
+APP_CONFIG = cfg.fetch
---
+++ spec/lib/portus/config_spec.rb
@@ -0,0 +1,37 @@
+# Returns a Portus::Config configured with the two given config files.
+def get_config(default, local)
+  default = File.join(Rails.root, "spec", "fixtures", default)
+  local   = File.join(Rails.root, "spec", "fixtures", local)
+  Portus::Config.new(default, local)
+end
+
+describe Portus::Config, focus: true do
+  it "returns an empty config if neither the global nor the local were found" do
+    cfg = get_config("", "").fetch
+
+    expect(cfg.enabled?("gravatar")).to be_falsey
+    expect(cfg.enabled?("ldap")).to be_falsey
+  end
+
+  it "only uses the global if the local config was not found" do
+    cfg = get_config("config.yml", "").fetch
+
+    expect(cfg.enabled?("gravatar")).to be_truthy
+    expect(cfg.enabled?("ldap")).to be_falsey
+  end
+
+  it "merges both config files and work as expected" do
+    cfg = get_config("config.yml", "local.yml").fetch
+
+    expect(cfg.enabled?("gravatar")).to be_truthy
+    expect(cfg.enabled?("ldap")).to be_truthy
+    expect(cfg["ldap"]["hostname"]).to eq "ldap.example.com"
+    expect(cfg["ldap"]["port"]).to eq 389
+    expect(cfg["ldap"]["base"]).to eq "ou=users,dc=example,dc=com"
+  end
+
+  it "raises an error when the local file is badly formatted" do
+    bad   = get_config("config.yml", "bad.yml")
+    expect { bad.fetch }.to raise_error(StandardError, "Wrong format for the config-local file!")
+  end
+end
