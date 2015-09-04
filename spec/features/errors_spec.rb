81d61500d36c130f15da467d3f5ea0bf6f2e340c
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2015-09-04 09:48:04 +0200

    review errors_spec to be safe
    use "begin ... ensure" to make sure the "after" block is run after
    running the example

--- spec/features/errors_spec.rb
+++ spec/features/errors_spec.rb
@@ -4,13 +4,16 @@ feature "custom error handler page" do
   describe "with custom handler setup" do
 
     around :each do |example|
-      Rails.application.config.consider_all_requests_local = false
-      Rails.application.config.action_dispatch.show_exceptions = true
-      Rails.env = ActiveSupport::StringInquirer.new("production")
-      example.run
-      Rails.application.config.consider_all_requests_local = true
-      Rails.application.config.action_dispatch.show_exceptions = false
-      Rails.env = ActiveSupport::StringInquirer.new("test")
+      begin
+        Rails.application.config.consider_all_requests_local = false
+        Rails.application.config.action_dispatch.show_exceptions = true
+        Rails.env = ActiveSupport::StringInquirer.new("production")
+        example.run
+      ensure
+        Rails.application.config.consider_all_requests_local = true
+        Rails.application.config.action_dispatch.show_exceptions = false
+        Rails.env = ActiveSupport::StringInquirer.new("test")
+      end
     end
 
     scenario "when no permissions routes to custom error page" do
