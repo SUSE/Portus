SUSE/Portus

commit 09dc052721ad560e4fb56c585f6193d3305fc578

Added SSL support for the communication between Portus and the registry
This fixes some known issues like the setup of the appliance, in which the
Portus instance and the registry just couldn't speak to each other.

In order to do this, the `use_ssl` attribute has been introduced. This
attribute can be changed in the "registries#show" page in the form of a toggle
box. There's also a checkbox that allows the admin to set `use_ssl` as true
when creating a new registry.

Moreover, this commit also brings some needed cleaning in regards to classes
dealing with registries.

Fixes #362

Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

app/models/registry.rb
@@ -1,8 +1,17 @@
+# Registry holds data regarding the registries registered in the Portus
+# application.
+#
+# NOTE: currently only one Registry is allowed to exist in the database. This
+# might change in the future.
 class Registry < ActiveRecord::Base
   has_many :namespaces
+
   validates :name, presence: true, uniqueness: true
   validates :hostname, presence: true, uniqueness: true
+  validates :use_ssl, inclusion: [true, false]
 
+  # Creates a global namespace owned by this registry. This method is called
+  # whenever a new registry is saved.
   def create_global_namespace!
     team = Team.create(
       name:   Namespace.sanitize_name(hostname),
@@ -16,8 +25,15 @@ class Registry < ActiveRecord::Base
       team:     team)
   end
 
+  # Returns the global namespace owned by this registry.
   def global_namespace
     Namespace.find_by(registry: self, global: true)
+  end
+
+  # Returns a registry client based on this registry that authenticates with
+  # the credentials of the "portus" user.
+  def client
+    Portus::RegistryClient.new(hostname, use_ssl)
   end
 
   # Find the registry for the given push event.
@@ -56,20 +72,18 @@ class Registry < ActiveRecord::Base
     [namespace, repo, tag_name]
   end
 
+  protected
+
   # Fetch the tag of the image contained in the current event. The Manifest API
   # is used to fetch it, thus the repo name and the digest are needed (and
   # they are contained inside the event's target).
   #
   # Returns the name of the tag if found, nil otherwise.
   def get_tag_from_manifest(target)
-    client = Portus::RegistryClient.new(hostname)
+    man = client.manifest(target["repository"], target["digest"])
+    man["tag"]
 
-    begin
-      man = client.manifest(target["repository"], target["digest"])
-      man["tag"]
-    rescue
-      logger.error("Could not fetch the tag for target #{target}.")
-      return
-    end
+  rescue
+    logger.error("Could not fetch the tag for target #{target}.")
   end
 end

spec/models/registry_spec.rb
@@ -1,7 +1,11 @@
 require "rails_helper"
 
-RSpec.describe Registry, type: :model do
+# Open up Portus::RegistryClient to inspect some attributes.
+Portus::RegistryClient.class_eval do
+  attr_reader :host, :use_ssl, :base_url, :username, :password
+end
 
+RSpec.describe Registry, type: :model do
   it { should have_many(:namespaces) }
 
   describe "#create_global_namespace" do
@@ -15,6 +19,20 @@ RSpec.describe Registry, type: :model do
 
       expect(owners.count).to be(2)
       expect(users).to match_array(owners)
+    end
+  end
+
+  describe "#client" do
+    let!(:registry) { create(:registry, use_ssl: true) }
+
+    it "returns a client with the proper config" do
+      client = registry.client
+
+      expect(client.host).to eq registry.hostname
+      expect(client.use_ssl).to be_truthy
+      expect(client.base_url).to eq "https://#{registry.hostname}/v2/"
+      expect(client.username).to eq "portus"
+      expect(client.password).to eq Rails.application.secrets.portus_password
     end
   end
 end

app/controllers/admin/registries_controller.rb
@@ -1,4 +1,9 @@
+# Allows the creation of exactly one registry. It also allows updating the
+# "use_ssl" attribute of a given registry. Doing all this is safe because only
+# admin users will be able to reach this controller.
 class Admin::RegistriesController < Admin::BaseController
+  before_action :registry_created, only: [:new, :create]
+
   # GET /admin/registries/
   def index
     @registries = Registry.all
@@ -11,12 +16,6 @@ class Admin::RegistriesController < Admin::BaseController
 
   # POST /admin/registries
   def create
-    if Registry.count > 0
-      redirect_to admin_registries_path,
-        alert: "No more than one registry is currently supported"
-      return
-    end
-
     @registry = Registry.new(registries_params)
 
     if @registry.save
@@ -27,9 +26,27 @@ class Admin::RegistriesController < Admin::BaseController
     end
   end
 
+  # PUT /admin/registries/1
+  #
+  # Right now this just toggles the value of the "use_ssl" attribute for the
+  # given registry. This might change in the future.
+  def update
+    @registry = Registry.find(params[:id])
+
+    @registry.update_attributes(use_ssl: !@registry.use_ssl?)
+    render template: "admin/registries/update", locals: { registry: @registry }
+  end
+
   private
 
+  # Raises a routing error if there is already a registry in place.
+  # NOTE: (mssola) remove this once we support multiple registries.
+  def registry_created
+    raise ActionController::RoutingError, "Not found" if Registry.count > 0
+  end
+
+  # The required/permitted parameters on the create method.
   def registries_params
-    params.require(:registry).permit(:name, :hostname)
+    params.require(:registry).permit(:name, :hostname, :use_ssl)
   end
 end

app/jobs/catalog_job.rb
@@ -11,8 +11,7 @@ class CatalogJob < ActiveJob::Base
     return if registry.nil?
 
     begin
-      client = Portus::RegistryClient.new(registry.hostname)
-      cat = client.catalog
+      cat = registry.client.catalog
 
       # Update the registry in a transaction, since we don't want to leave the DB
       # in an unknown state because of an update failure.

app/views/admin/registries/index.html.slim
@@ -20,11 +20,20 @@
             tr
               th Name
               th Hostname
+              th SSL
           tbody
             - @registries.each do |registry|
-              tr
+              tr id="registry_#{registry.id}"
                 td= registry.name
                 td= registry.hostname
+                td
+                  a.btn.btn-default[data-remote="true"
+                      data-method="put"
+                      rel="nofollow"
+                      href=url_for(admin_registry_path(registry))
+                      title="Enable/disable SSL in the communication between Portus and this registry"
+                      ]
+                      i.fa.fa-lg class="fa-toggle-#{registry.use_ssl? ? 'on': 'off'}"
 
   p
     strong Note well:

app/views/admin/registries/new.html.slim
@@ -10,6 +10,10 @@ h1 New Registry
     .col-md-7
       = f.text_field(:hostname, class: 'form-control', placeholder: 'registry.test.lan', required: true)
   .form-group
+    = f.label :use_ssl, "Use SSL", {class: 'control-label col-md-2', title: 'Set this to enable SSL in the communication between Portus and the Registry'}
+    .col-md-7
+      = f.check_box(:use_ssl)
+  .form-group
     .col-md-offset-2.col-md-7
       = f.submit('Create', class: 'btn btn-primary')
 

app/views/admin/registries/update.js.erb
@@ -1,0 +1,12 @@
+<% if @registry.errors.empty? %>
+  $('#alert p').html("Registry '<%= @registry.name %>' is <%= @registry.use_ssl? ? 'now using SSL' : 'not using SSL anymore' %>.");
+  $('#alert .alert').removeClass('alert-danger')
+
+  $('#registry_<%= @registry.id %> td a i').addClass("fa-toggle-<%= @registry.use_ssl? ? 'on' : 'off' %>");
+  $('#registry_<%= @registry.id %> td a i').removeClass("fa-toggle-<%= @registry.use_ssl? ? 'off' : 'on' %>");
+<% else %>
+  $('#alert p').html("Selected registry does not exist!");
+  $('#alert .alert').addClass('alert-danger')
+<% end %>
+
+$('#alert').fadeIn();

bin/client.rb
@@ -8,17 +8,21 @@
 #   - delete <name> <digest>
 #   - manifest <name>[:<tag>]
 
-client = Portus::RegistryClient.new("registry.test.lan")
+registry = Registry.first
+if registry.nil?
+  puts "No registry has been configured!"
+  exit 1
+end
 
 case ARGV.first
 when "catalog"
-  pp client.catalog
+  pp registry.client.catalog
 when "delete"
   if ARGV.length == 2
     puts "You have to specify first the name, and then the digest"
     exit 1
   end
-  pp client.delete(ARGV[1], ARGV[2])
+  pp registry.client.delete(ARGV[1], ARGV[2])
 when "manifest"
   if ARGV.length == 1
     puts "You have to at least specify the name of the image"
@@ -30,7 +34,7 @@ when "manifest"
   else
     name, tag = ARGV[1], "latest"
   end
-  pp client.manifest(name, tag)
+  pp registry.client.manifest(name, tag)
 else
   puts "Valid commands: catalog, delete, manifest."
   exit 1

commit 71c84b87f8a77f1359c1641e213fcffd7c6ab45c

registry: return the proper error when fetching the manifest fails
As noted by @morodin in the issue #338, when Portus fails the fetch the
manifest, it adds a new tag "1" into the database. This is due to a regression
introduced in 09dc052721ad560e4fb56c585f6193d3305fc578. In this case, on
failure the `get_tag_from_manifest` method returned true on error, because
that's what the logger returns. This commit makes sure that in this case nil is
returned.

Moreover, Portus will now also log the error that has happenned when fetching
the manifest.

Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

app/models/registry.rb
@@ -71,8 +71,10 @@ class Registry < ActiveRecord::Base
     man = client.manifest(target["repository"], target["digest"])
     man["tag"]
 
-  rescue
-    logger.error("Could not fetch the tag for target #{target}.")
+  rescue StandardError => e
+    logger.info("Could not fetch the tag for target #{target}")
+    logger.info("Reason: #{e.message}")
+    nil
   end
 
   # Create the global namespace for this registry and create the personal

spec/models/registry_spec.rb
@@ -5,6 +5,33 @@ Portus::RegistryClient.class_eval do
   attr_reader :host, :use_ssl, :base_url, :username, :password
 end
 
+# Mock class that returns a client that can fail depending on how this class is
+# initialized.
+class RegistryMock < Registry
+  def initialize(should_fail)
+    @should_fail = should_fail
+  end
+
+  def client
+    o = nil
+    if @should_fail
+      def o.manifest(*_)
+        raise StandardError, "Some message"
+      end
+    else
+      def o.manifest(*_)
+        { "tag" => "latest" }
+      end
+    end
+    o
+  end
+
+  def get_tag_from_manifest_test(repo, digest)
+    target = { repository: repo, digest: digest }
+    get_tag_from_manifest(target)
+  end
+end
+
 RSpec.describe Registry, type: :model do
   it { should have_many(:namespaces) }
 
@@ -44,6 +71,25 @@ RSpec.describe Registry, type: :model do
       expect(client.base_url).to eq "https://#{registry.hostname}/v2/"
       expect(client.username).to eq "portus"
       expect(client.password).to eq Rails.application.secrets.portus_password
+    end
+  end
+
+  describe "#get_tag_from_manifest" do
+    it "returns a tag on success" do
+      mock = RegistryMock.new(false)
+
+      ret = mock.get_tag_from_manifest_test("busybox", "sha:1234")
+      expect(ret).to eq "latest"
+    end
+
+    it "handles errors properly" do
+      mock = RegistryMock.new(true)
+
+      expect(Rails.logger).to receive(:info).with(/Could not fetch the tag/)
+      expect(Rails.logger).to receive(:info).with(/Reason: Some message/)
+
+      ret = mock.get_tag_from_manifest_test("busybox", "sha:1234")
+      expect(ret).to be_nil
     end
   end
 end

spec/models/repository_spec.rb
@@ -37,14 +37,12 @@ describe Repository do
   end
 
   describe "handle push event" do
-
     let(:tag_name) { "latest" }
     let(:repository_name) { "busybox" }
     let(:registry) { create(:registry, hostname: "registry.test.lan") }
     let(:user) { create(:user) }
 
     context "event does not match regexp of manifest" do
-
       let(:event) do
         e = build(:raw_push_manifest_event).to_test_hash
         e["target"]["repository"] = repository_name
@@ -55,14 +53,11 @@ describe Repository do
 
       it "sends event to logger" do
         VCR.use_cassette("registry/get_image_manifest_webhook", record: :none) do
-          error_msg = "Could not fetch the tag for target"
-          expect(Rails.logger).to receive(:error).with(/^#{error_msg}/)
           expect do
             Repository.handle_push_event(event)
           end.to change(Repository, :count).by(0)
         end
       end
-
     end
 
     context "event comes from an unknown registry" do
@@ -92,7 +87,6 @@ describe Repository do
       end
 
       it "sends event to logger" do
-        expect(Rails.logger).to receive(:error)
         expect do
           Repository.handle_push_event(@event)
         end.to change(Repository, :count).by(0)