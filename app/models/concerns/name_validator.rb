97435f20d82bd16cadd68824cbfe85ef2e365b3c
Miquel Sabaté Solà <msabate@suse.com>
2015-10-07 15:43:26 +0200

    Abide to Docker's naming rules for namespaces, repositories and tags
    Fixes #400
    
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

--- app/models/concerns/name_validator.rb
+++ app/models/concerns/name_validator.rb
@@ -1,7 +1,10 @@
+# Include this concern to validate the `name` attribute of the model according
+# to the rules defined in registry/api/v2/names.go from Docker's Distribution
+# project.
 module NameValidator
   extend ActiveSupport::Concern
 
-  NAME_ALLOWED_CHARS = 'a-z0-9\-_'
+  NAME_ALLOWED_CHARS = "[a-z0-9]+(?:[._-][a-z0-9]+)*"
 
   included do
     key = name == "Namespace" ? "registry" : "namespace"
@@ -9,8 +12,10 @@ module NameValidator
     validates :name,
               presence:   true,
               uniqueness: { scope: "#{key}_id" },
+              length:     { maximum: 255 },
               format:     {
-                with:    /\A[#{NAME_ALLOWED_CHARS}]+\Z/,
-                message: "Only allowed letters: [a-z0-9-_]" }
+                with:    /\A#{NAME_ALLOWED_CHARS}+\Z/,
+                message: "must match: #{NAME_ALLOWED_CHARS}"
+              }
   end
 end
