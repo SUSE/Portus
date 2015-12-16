58ac73851a9cf68af829569d9303fafb2a5efd48
Aleksa Sarai <asarai@suse.com>
2015-12-16 23:54:53 +1100

    compose: support Docker 1.9 overlay networking
    Rather than using the (quite outdated) links system, use the new overlay
    network driver added in Docker 1.9. While this support is currently (as
    of Docker compose 1.5.2) experimental, it works pretty well.
    
    Signed-off-by: Aleksa Sarai <asarai@suse.com>

--- docker/registry/entry.sh
+++ docker/registry/entry.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [ ! -e /registry/config.yml ]; then
-  sed -e "s|DOCKER_HOST|${DOCKER_HOST}|g" /registry/config.yml.template > /registry/config.yml
+  sed -e "{
+    s|DOCKER_HOST|${DOCKER_HOST}|g;
+    s|PORTUS_WEB_HOST|${PORTUS_WEB_HOST}|g;
+  }" /registry/config.yml.template > /registry/config.yml
 fi
 
 registry /registry/config.yml
