9584d0a80323a1f83b27414da4672e9af0d42e28
Miquel Sabaté Solà <msabate@suse.com>
2016-05-06 10:06:54 +0200

    user: be more explicit on the allowed name format
    bsc#978658
    
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true,
     format: {
       with:    USERNAME_FORMAT,
-      message: "Only alphanumeric characters are allowed. Minimum 4 characters, maximum 30."
+      message: "only lower case alphanumeric characters are allowed. Minimum 4 characters, maximum 30."
     }
 
   # Actions performed before/after create.
