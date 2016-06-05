a72a4f5eefea0099db73340db19a48694ebdb72a
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-06-05 17:50:12 -0300

    New menu layout on small screens
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- assets/js/topbar.js
+++ assets/js/topbar.js
@@ -1,15 +1,20 @@
 //change topbar background
-$(window).on('scroll', function() {
+function setTopbarBackground() {
   var welcome_height = $('#welcome').outerHeight();
   var topbar_height = $('#portus-header').outerHeight();
-  if ($(window).scrollTop() > welcome_height - topbar_height) {
-    $("#portus-header").addClass("opaque");
-    $("#mobile-menu").addClass("opaque");
-  } else {
-    $("#portus-header").removeClass("opaque");
-    $("#mobile-menu").removeClass("opaque");
+  if (!menu_open) {
+    if ($(window).scrollTop() > welcome_height - topbar_height) {
+      $("#portus-header").addClass("opaque");
+      $("#mobile-menu").addClass("opaque");
+    } else {
+      $("#portus-header").removeClass("opaque");
+      $("#mobile-menu").removeClass("opaque");
+    }
   }
-})
+};
+$(window).on('scroll', function() {
+  setTopbarBackground();
+});
 
 // Set topbar opaque if the page haven't a welcome image
 $(document).ready(function() {
@@ -20,17 +25,21 @@ $(document).ready(function() {
     }
 })
 
-
 // Functions for the mobile version
 $(document).on("click", '#open_main_menu', open_mobile_menu);
 $("#mobile-menu").hide();
 var menu_open = false
 function open_mobile_menu () {
+  var has_opaque = $("#portus-header").hasClass('opaque');
   if (menu_open) {
-    $("#mobile-menu").css({'opacity': '0'});
     menu_open = false
+    $("#mobile-menu").removeClass('active');
+    setTopbarBackground();
   } else {
-    $("#mobile-menu").css({'opacity': '1'});
+    $("#mobile-menu").addClass('active');
+    if (!has_opaque) {
+      $("#portus-header").addClass("opaque");
+    }
     menu_open = true;
   }
 }
