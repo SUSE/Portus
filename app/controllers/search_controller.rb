9dea612183a7ac1de9772534ce5207e38fd77992
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-07-29 22:34:01 -0300

    Initial search dynamic filter
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,19 @@
 class SearchController < ApplicationController
   def index
     search = params[:search].split(":").first
-    @repositories = policy_scope(Repository).search(search)
-    @teams = policy_scope(Team).search(search)
-    @namespaces = policy_scope(Namespace).search(search)
+    @search_query = params[:search]
+    @search_type = params[:type]
+
+    repositories = policy_scope(Repository).search(search)
+    @repositories = repositories.page(params[:page]).per(2)
+    @repositories_count = repositories.count()
+
+    teams = policy_scope(Team).search(search)
+    @teams = teams.page(params[:page]).per(2)
+    @teams_count = teams.count()
+
+    namespaces = policy_scope(Namespace).search(search)
+    @namespaces = namespaces.page(params[:page]).per(2)
+    @namespaces_count = namespaces.count()
   end
 end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,5 +1,5 @@
 module SearchHelper
   def build_search_category_url(params, category)
-    "#{search_index_path}?utf8=#{params[:utf8]}&search=#{params[:search]}&type=#{category}"
+    "#{search_index_path}?utf8=#{params[:utf8]}&search=#{params[:search]}&page=#{params[:page]}&type=#{category}"
   end
 end
