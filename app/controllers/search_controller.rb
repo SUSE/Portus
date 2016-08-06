f892b8a79b4db79fb04e7d2fa12e07c0e20437d9
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-08-05 22:32:28 -0300

    Removed unnecessary thing to filtering
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,15 +5,15 @@ class SearchController < ApplicationController
     @search_type = params[:type]
 
     repositories = policy_scope(Repository).search(search)
-    @repositories = repositories.page(params[:page]).per(2)
+    @repositories = repositories.page(params[:page])
     @repositories_count = repositories.count()
 
     teams = policy_scope(Team).search(search)
-    @teams = teams.page(params[:page]).per(2)
+    @teams = teams.page(params[:page])
     @teams_count = teams.count()
 
     namespaces = policy_scope(Namespace).search(search)
-    @namespaces = namespaces.page(params[:page]).per(2)
+    @namespaces = namespaces.page(params[:page])
     @namespaces_count = namespaces.count()
   end
 end
