58d1df7bc82d2ce8c98ae490b57ee3494db115f2
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-08-12 19:48:53 -0300

    Added filter to admin_namespaces#index
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- app/controllers/admin/namespaces_controller.rb
+++ app/controllers/admin/namespaces_controller.rb
@@ -1,6 +1,9 @@
 class Admin::NamespacesController < Admin::BaseController
+  respond_to :html, :js
+
   def index
     @special_namespaces = Namespace.where(global: true)
-    @namespaces = Namespace.not_portus.where(global: false).page(params[:page])
+    @namespaces = Namespace.not_portus.where(global: false).search(params[:filter]).page(params[:page])
+    respond_with(@namespaces)
   end
 end
