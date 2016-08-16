928aeb69d8eca9f6c9c878aab457126444a58222
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-08-16 19:48:17 -0300

    Added filter to admin_teams#index
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- app/controllers/admin/teams_controller.rb
+++ app/controllers/admin/teams_controller.rb
@@ -1,5 +1,7 @@
 class Admin::TeamsController < Admin::BaseController
+  respond_to :html, :js
+
   def index
-    @teams = Team.all_non_special.page(params[:page])
+    @teams = Team.all_non_special.search(params[:filter]).page(params[:page])
   end
 end
