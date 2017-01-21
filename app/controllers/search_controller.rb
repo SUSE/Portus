7b1161586c778de92399bfc1035e24f5cea720c3
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-01-21 20:06:14 -0200

    Some refactoring
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,16 +4,8 @@ class SearchController < ApplicationController
     @search_query = params[:search]
     @search_type = params[:type]
 
-    repositories = policy_scope(Repository).search(search)
-    @repositories = repositories.page(params[:page])
-    @repositories_count = repositories.count
-
-    teams = policy_scope(Team).search(search)
-    @teams = teams.page(params[:page])
-    @teams_count = teams.count
-
-    namespaces = policy_scope(Namespace).search(search)
-    @namespaces = namespaces.page(params[:page])
-    @namespaces_count = namespaces.count
+    @repositories = policy_scope(Repository).search(search).page(params[:page])
+    @teams = policy_scope(Team).search(search).page(params[:page])
+    @namespaces = policy_scope(Namespace).search(search).page(params[:page])
   end
 end
