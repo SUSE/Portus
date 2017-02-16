3b7e8c4d3d887013fadcb9c7f70ea72ea19481fb
Miquel Sabaté Solà <msabate@suse.com>
2017-02-16 13:31:25 +0100

    Fixed rubocop complains
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env puma
 
 # Workers and connections.
-threads 1, (ENV["PORTUS_PUMA_MAX_THREADS"] || 1)
-workers (ENV["PORTUS_PUMA_WORKERS"] || 1)
+threads 1, ENV["PORTUS_PUMA_MAX_THREADS"] || 1
+workers ENV["PORTUS_PUMA_WORKERS"] || 1
 
 # If specified, use the given host:port. Otherwise, we will use a local
 # deployment through a UNIX socket.
