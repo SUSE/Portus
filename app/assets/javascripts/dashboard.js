de1d3e8345925b728b99f6d0caf23f1701dfa379
Hart Simha <hart.simha@suse.com>
2017-03-27 15:30:51 -0700

    Fix dashboard sidebar links
    Signed-off-by: Hart Simha <hart.simha@suse.com>

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,15 +1,5 @@
 jQuery(function ($) {
-  $('#starred a').on('click', function (e) {
-    e.preventDefault();
-    $(this).tab('show');
-  });
-
-  $('#all a').on('click', function (e) {
-    e.preventDefault();
-    $(this).tab('show');
-  });
-
-  $('#personal a').on('click', function (e) {
+  $('#sidebar-tabs a').on('click', function (e) {
     e.preventDefault();
     $(this).tab('show');
   });
