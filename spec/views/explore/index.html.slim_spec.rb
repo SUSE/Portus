767ba7620e470c38df6cee3d20b360f2aed50c24
Miquel Sabaté Solà <msabate@suse.com>
2017-08-09 13:24:56 +0200

    Implemented anonymous browsing
    This will allow (through a configurable option) anonymous users to
    explore public images from the configured Docker registry.
    
    Fixes #1129
    
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

---
+++ spec/views/explore/index.html.slim_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+describe "explore/index", type: :view do
+  it "renders the page successfully" do
+    @repositories = []
+    render
+    assert_select("#explore_search")
+  end
+end
