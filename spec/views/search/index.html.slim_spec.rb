d12306daa47bb78142b969650bfd306087dc1d10
Miquel Sabaté Solà <msabate@suse.com>
2017-08-31 11:25:14 +0200

    Fixed a render error on the search/index page
    I've also provided a view spec test.
    
    Fixes #1398
    
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

---
+++ spec/views/search/index.html.slim_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe "search/index" do
+  context "regular render" do
+    let!(:registry)   { create(:registry, hostname: "registry.test.lan") }
+    let!(:user)       { create(:admin) }
+    let!(:team)       { create(:team, owners: [user]) }
+    let!(:namespace)  { create(:namespace, team: team, name: "user") }
+    let!(:repository) { create(:repository, namespace: namespace, name: "busybox") }
+
+    before do
+      @teams        = Team.all
+      @namespaces   = Namespace.all
+      @repositories = Repository.all
+    end
+
+    # See https://github.com/SUSE/Portus/issues/1398
+    it "renders the page successfully" do
+      render
+
+      section = assert_select(".panel-heading")
+      expect(section.text).to eq "Search results"
+    end
+  end
+end
