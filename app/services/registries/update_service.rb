9aa3ee218ffd0e2d48f223ad1c7b893b9301f5c1
Miquel Sabaté Solà <msabate@suse.com>
2018-02-07 14:30:27 +0100

    api: added create and update methods to registries
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

---
+++ app/services/registries/update_service.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Registries
+  class UpdateService < ::Registries::BaseService
+    def execute
+      @registry = Registry.find(params[:id])
+      @messages = @registry.errors.messages
+      @registry.assign_attributes(params)
+      @valid = @registry.valid?
+
+      check!
+      if @valid
+        @messages.merge(@registry.errors.messages) unless @registry.save
+      end
+
+      @registry
+    end
+
+    protected
+
+    def check!
+      return unless @valid
+      check_hostname!
+      return unless @valid
+      check_reachability! unless @force
+    end
+
+    def check_hostname!
+      return if !Repository.any? || params[:hostname].blank?
+      @valid = false
+      @messages[:hostname] = "Registry is not empty, cannot change hostname"
+    end
+  end
+end
