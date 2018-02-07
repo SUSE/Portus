5a16f2094bbf4e96e83dd076124971ef04e49d2d
Miquel Sabaté Solà <msabate@suse.com>
2018-02-07 14:30:27 +0100

    api: added create and update methods to registries
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

--- lib/api/v1/registries.rb
+++ lib/api/v1/registries.rb
@@ -25,6 +25,77 @@ module API
           present Registry.all, with: API::Entities::Registries
         end
 
+        desc "Create a registry",
+             tags:     ["registries"],
+             detail:   "Allow users to create a registry. " \
+                       "This will only work if no registry works yet",
+             entity:   API::Entities::Registries,
+             consumes: ["application/x-www-form-urlencoded", "application/json"],
+             failure:  [
+               [400, "Bad request", API::Entities::ApiErrors],
+               [401, "Authentication fails"],
+               [403, "Authorization fails"]
+             ]
+
+        params do
+          requires :registry, type: Hash do
+            requires :all,
+                     only:  %i[name hostname use_ssl],
+                     using: API::Entities::Registries.documentation.slice(
+                       :name, :hostname, :use_ssl
+                     )
+            optional :all,
+                     only:  %i[use_ssl external_hostname],
+                     using: API::Entities::Registries.documentation.slice(:external_hostname)
+          end
+        end
+
+        post do
+          svc = ::Registries::CreateService.new(current_user, permitted_params[:registry])
+          obj = svc.execute
+
+          if svc.valid?
+            present obj, with: API::Entities::Registries
+          else
+            status 400
+            { errors: svc.messages }
+          end
+        end
+
+        desc "Update registry",
+             params:   API::Entities::Registries.documentation.slice(:id),
+             failure:  [
+               [400, "Bad request", API::Entities::ApiErrors],
+               [401, "Authentication fails"],
+               [403, "Authorization fails"],
+               [404, "Not found"]
+             ],
+             entity:   API::Entities::Registries,
+             consumes: ["application/x-www-form-urlencoded", "application/json"]
+
+        params do
+          requires :registry, type: Hash do
+            optional :all,
+                     only:  %i[name hostname use_ssl external_hostname],
+                     using: API::Entities::Registries.documentation.slice(
+                       :name, :hostname, :use_ssl, :external_hostname
+                     )
+          end
+        end
+
+        put ":id" do
+          attrs = declared(params, include_missing: false)[:registry].merge(id: params[:id])
+          svc = ::Registries::UpdateService.new(current_user, attrs)
+          obj = svc.execute
+
+          if svc.valid?
+            present obj, with: API::Entities::Registries
+          else
+            status 400
+            { errors: svc.messages }
+          end
+        end
+
         desc "Validates the given registry",
              tags:    ["registries"],
              detail:  "Besides containing the usual Status object, it adds the reachable " \
