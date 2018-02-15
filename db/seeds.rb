32a4d082294f8eb19062e2f7709cac944d23b603
Miquel Sabaté Solà <msabate@suse.com>
2018-02-15 16:21:01 +0100

    spec: nuked previous integration tests
    They were buggy and we need to take another approach.
    
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,25 +14,3 @@ User.create!(
   email:    "portus@portus.com",
   admin:    true
 )
-
-# Adding a user and a registry for integration tests.
-if ENV["INTEGRATION_TESTS"]
-  Rails.logger.info "Adding user username31"
-  if User.find_by(username: "username31")
-    Rails.logger.fatal "User already exists. Please drop the database first"
-    exit(-1)
-  end
-
-  User.create(
-    username: "username31",
-    email:    "a1@b.com",
-    password: "test-password",
-    admin:    true
-  )
-
-  Rails.logger.info "Adding registry portus.suse.example.com:5000"
-  Registry.create(
-    name:     "portus.suse.example.com:5000",
-    hostname: "portus.suse.example.com:5000"
-  )
-end
