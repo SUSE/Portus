5897c773f39df90838665e7a0012f6827c3c5693
Vítor Avelino <vavelino@suse.com>
2018-06-06 15:24:41 -0300

    js: refactor data -> components way on repositories#index
    Signed-off-by: Vítor Avelino <vavelino@suse.com>

--- app/assets/javascripts/modules/repositories/index.js
+++ app/assets/javascripts/modules/repositories/index.js
@@ -1,2 +1,20 @@
-import './pages/index';
+import Vue from 'vue';
+
+import RepositoriesIndexPage from './pages/index';
+
 import './pages/show';
+
+$(() => {
+  if (!$('body[data-route="repositories/index"]').length) {
+    return;
+  }
+
+  // eslint-disable-next-line no-new
+  new Vue({
+    el: '.vue-root',
+
+    components: {
+      RepositoriesIndexPage,
+    },
+  });
+});
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,15 +7,20 @@ class RepositoriesController < ApplicationController
   # GET /repositories.json
   def index
     @repositories = policy_scope(Repository)
-    @repositories_serialized = API::Entities::Repositories.represent(
-      @repositories,
-      current_user: current_user,
-      type:         :internal
-    ).to_json
     @team_repositories = Repository
                          .joins(namespace: { team: :users })
                          .where("users.id = :user_id", user_id: current_user.id)
-                         .map(&:full_name)
+    @other_repositories = @repositories - @team_repositories
+    @team_repositories_serialized = API::Entities::Repositories.represent(
+      @team_repositories,
+      current_user: current_user,
+      type:         :internal
+    ).to_json
+    @other_repositories_serialized = API::Entities::Repositories.represent(
+      @other_repositories,
+      current_user: current_user,
+      type:         :internal
+    ).to_json
   end
 
   # GET /repositories/1
