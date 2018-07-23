1bd967039787b072607485d0e8abc62e2a3361c8
Miquel Sabaté Solà <msabate@suse.com>
2018-07-23 13:12:47 +0200

    passwords: don't allow the portus user to reset
    This commit disallows someone knowing the portus email from attempting
    to reset the password through the "I forgot my password" feature. This
    attack wouldn't work anyways because of the change introduced in
    16a7b11095ae4dc641805cfcaed08366bb85d3cd, where updating the portus user
    is explicitly disabled (except when changing the secret for the
    password).
    
    See #1878
    
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

--- spec/features/forgotten_password_spec.rb
+++ spec/features/forgotten_password_spec.rb
@@ -3,7 +3,8 @@
 require "rails_helper"
 
 describe "Forgotten password support" do
-  let!(:user) { create(:admin) }
+  let!(:user)   { create(:admin) }
+  let!(:portus) { create(:admin, username: "portus", email: "portus@portus.com") }
 
   before do
     APP_CONFIG["signup"] = { "enabled" => true }
@@ -19,6 +20,14 @@ describe "Forgotten password support" do
   it "gives the user a link to reset their password" do
     visit new_user_session_path
     expect(page).to have_content("I forgot my password")
+  end
+
+  it "prevents the portus user from resetting the password" do
+    visit new_user_password_path
+
+    fill_in "Email", with: "portus@portus.com"
+    click_button "Reset password"
+    expect(page).to have_content("Action not allowed on this user")
   end
 
   it "sends the reset email when appropiate" do
