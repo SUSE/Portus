46876761a25e9ac2c6cf0d28ae9b5f4801dbf289
Vítor Avelino <vavelino@suse.com>
2018-08-03 12:45:31 -0300

    js: created disable-account-panel and cleanup
    The creation of this component made us able to get rid of another js
    dependency: jquery-ujs.
    
    Templates not being used anymore were also removed.
    
    Signed-off-by: Vítor Avelino <vavelino@suse.com>

---
+++ app/assets/javascripts/modules/users/components/disable-account-panel.vue
@@ -0,0 +1,37 @@
+<template>
+  <panel>
+    <h5 slot="heading-left">Disable account</h5>
+
+    <div slot="body">
+      <p>By disabling the account, you won't be able to access Portus with it, and any affiliations with any team will be lost.</p>
+      <button type="button" class="btn btn-primary" @click.prevent="disable">Disable</button>
+    </div>
+  </panel>
+</template>
+
+<script>
+  import Panel from '~/shared/components/panel';
+
+  import { handleHttpResponseError } from '~/utils/http';
+
+  import UsersService from '../service';
+
+  export default {
+    props: {
+      userId: Number,
+    },
+
+    components: {
+      Panel,
+    },
+
+    methods: {
+      disable() {
+        UsersService.toggleEnabled({ id: this.userId }).then(() => {
+          this.$alert.$schedule("You've successfully disabled your own user account.");
+          window.location.href = window.API_URL;
+        }).catch(handleHttpResponseError);
+      },
+    },
+  };
+</script>
